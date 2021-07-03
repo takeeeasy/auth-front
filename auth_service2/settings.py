25ae4aa904aaa9681e44872813bc6ff1f0685b44
takeeeasy <taks8238@gmail.com>
2021-07-04 02:08:07 +0900

    DATABASE SETTING 수정.

--- auth_service2/settings.py
+++ auth_service2/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'auth_service2.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'TSAUTH',
+        'NAME': 'TS25',
         'USER': 'dbmasteruser',
         'PASSWORD': 'devuser12#',
         'HOST': 'ls-09fb9ae9230de0f3dd768bb14371a25536da3870.cgsp0c9418c4.ap-northeast-2.rds.amazonaws.com',
