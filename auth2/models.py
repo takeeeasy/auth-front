9dd9ee1d41e6d48eb463f7450496821bc62fa067
takeeeasy <taks8238@gmail.com>
2021-07-04 10:38:13 +0900

    설정파일 분리.

--- auth2/models.py
+++ auth2/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 
 class User(models.Model):
-    userid = models.CharField(max_length=12)
+    userid = models.CharField(max_length=12, primary_key=True)
     username = models.CharField(max_length=12)
     password = models.CharField(max_length=20)
     email = models.CharField(max_length=20)
